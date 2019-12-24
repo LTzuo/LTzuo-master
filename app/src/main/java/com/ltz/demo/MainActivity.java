44043ed9f56d99b2cf0be558b15716838cd8b7c3
yangyang.zhai <yangyang.zhai@timanetworks.com>
2019-12-24 10:46:19 +0800

    创建项目

---
+++ app/src/main/java/com/ltz/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ltz.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * LTzuo-master
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
